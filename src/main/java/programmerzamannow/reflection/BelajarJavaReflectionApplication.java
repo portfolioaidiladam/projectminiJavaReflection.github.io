1ef01edce9058a9efc0c04deaa05bca22113b968
portfolioaidiladam <portfolioaidiladam@gmail.com>
2024-10-12 22:44:36 +0700

    first commit

---
+++ src/main/java/programmerzamannow/reflection/BelajarJavaReflectionApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.reflection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaReflectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaReflectionApplication.class, args);
+	}
+
+}
